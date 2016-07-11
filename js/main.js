646d8252be86b7e9bdd3b763a385435ff2f3f237
Kevin Wu <kevinwu-03@hotmail.com>
2016-07-11 16:06:49 -0400

    adsasda

--- js/main.js
+++ js/main.js
@@ -23,11 +23,11 @@ app.config(['$routeProvider', function ($routeProvider) {
     .when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
     .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
     .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
+    .when("/login", {templateUrl: "partials/login.html", controller: "PageCtrl"})
+    .when("/signup", {templateUrl: "partials/signup.html", controller: "PageCtrl"})
     // Blog
     .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
     .when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
-    .when("/blog/login", {templateUrl: "partials/login.html", controller: "PageCtrl"})
-    .when("/blog/signup", {templateUrl: "partials/signup.html", controller: "PageCtrl"})
     // else 404
     .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
