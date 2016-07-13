83cdb69616a7866fe8e4c5a586ccca1ce4e2fc2e
Kevin Wu <kevinwu-03@hotmail.com>
2016-07-12 21:24:24 -0400

    Fixed script import error and removed some things from testing

--- js/main.js
+++ js/main.js
@@ -154,10 +154,6 @@
   }
 });
 
-function redirectPage(){
-  if (lo) {}
-}
-
 $(document).on('click','.navbar-collapse.in',function(e) {
     if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
         $(this).collapse('hide');
